e27a62ee6dce755f1b16e9e37b02d06a8adc089e
albertofanini-prog <albertofanini@outlook.com>
2022-02-12 18:36:11 +0100

    6th Commit - Si vedono gli elementi inseriti con JS

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -21,3 +22,31 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+// //Creare variabili per i div
+// let previewBox = document.querySelector('.slider_preview');
+
+const sliderMain = document.querySelector('.slider_main');
+const sliderPreview = document.querySelector('.slider_preview');
+
+
+//Definire il punto di partenza
+let currentIndex = 0;
+
+//Creare il ciclo per l'immagine principale
+for(let i = 0; i < items.length; i++){
+    const mainBox = document.createElement('div');
+    const mainPic = `<img src="${items[i]}">`;
+    const divSliderMain = `<div class="slider_item"><h3>${title[i]}</h3><p>${text[i]}</p></div>`;
+    // const divSliderPreview = ;
+    mainBox.classList.add("list-item" + i)
+    mainBox.innerHTML += mainPic + divSliderMain;
+    sliderMain.append(mainBox);
+
+
+    if(i === currentIndex){
+        mainBox.classList.add('active');
+    }
+
+
+}
