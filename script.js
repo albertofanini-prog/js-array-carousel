9669d1b5de98bdc60e487bee2f83594ad48508bd
albertofanini-prog <albertofanini@outlook.com>
2022-02-19 12:46:32 +0100

    7th Commit - Rifatto e terminato

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Inserire in un array gli elementi da riprodurre
 
 const items = [
     'img/01.jpg',
@@ -6,7 +7,7 @@ const items = [
     'img/04.jpg',
     'img/05.jpg'
 ];
-
+  
 const title = [
     'Svezia',
     'Svizzera',
@@ -14,7 +15,7 @@ const title = [
     'Germania',
     'Paradise'
 ]
-
+  
 const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
     'Lorem ipsum',
@@ -22,31 +23,76 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
-
-// //Creare variabili per i div
-// let previewBox = document.querySelector('.slider_preview');
-
-const sliderMain = document.querySelector('.slider_main');
-const sliderPreview = document.querySelector('.slider_preview');
-
-
-//Definire il punto di partenza
-let currentIndex = 0;
-
-//Creare il ciclo per l'immagine principale
-for(let i = 0; i < items.length; i++){
-    const mainBox = document.createElement('div');
-    const mainPic = `<img src="${items[i]}">`;
-    const divSliderMain = `<div class="slider_item"><h3>${title[i]}</h3><p>${text[i]}</p></div>`;
-    // const divSliderPreview = ;
-    mainBox.classList.add("list-item" + i)
-    mainBox.innerHTML += mainPic + divSliderMain;
-    sliderMain.append(mainBox);
-
-
-    if(i === currentIndex){
-        mainBox.classList.add('active');
-    }
-
-
+  
+//Dichiarare dove inserire gli elementi
+const imgSmall = document.querySelector('div.img_small');
+const imgFull = document.querySelector('div.img_full');
+  
+let selected = 3;
+imgFull.innerHTML = `<img src="${items[selected]}">`
+  
+//Creare ciclo per inserire gli elementi
+for ( i = 0; i < items.length; i++ ){
+    const scrollImg = `
+        <div class="scrolling-image">
+             <img src="${items[i]}">
+        </div>
+    `
+    //Inserire in HTML
+    imgSmall.innerHTML += scrollImg
 }
+  
+//Lavorare su wrapper di destra
+const scrollWrapper = [...document.getElementsByClassName('scrolling-image')];
+scrollWrapper[selected].classList.add('active');
+  
+//Inserire elementi della descrizione
+const description = document.querySelector('div.description');
+  
+let imgTitle = title[selected];
+let imgText = text[selected];
+  
+description.innerHTML = `
+<h2>${imgTitle}</h2>
+<p>${imgText}</p>
+`; 
+  
+//Animare button slide precedente 
+const next = document.querySelector('.next')
+next.addEventListener('click', function(){
+    scrollWrapper[selected].classList.remove('active');
+    if (selected < items.length -1){
+        selected++
+    }
+    scrollWrapper[selected].classList.add('active');  
+    imgFull.innerHTML = `<img src="${items[selected]}">`
+  
+    imgTitle = title[selected];
+    imgText = text[selected];
+    
+    description.innerHTML = `
+    <h2>${imgTitle}</h2>
+    <p>${imgText}</p>
+    `; 
+})
+  
+//Annimare button slide successiva
+const prev = document.querySelector('.prev');
+  
+prev.addEventListener('click', function(){
+    scrollWrapper[selected].classList.remove('active');
+    if (selected > 0){
+        selected--
+    } 
+    scrollWrapper[selected].classList.add('active');   
+    imgFull.innerHTML = `<img src="${items[selected]}">`
+  
+    imgTitle = title[selected];
+    imgText = text[selected];
+  
+    description.innerHTML = `
+    <h2>${imgTitle}</h2>
+    <p>${imgText}</p>
+    `; 
+})
+    
