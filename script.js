0a64d65de4bb02d93d141658194f0209a34b2f2c
albertofanini-prog <albertofanini@outlook.com>
2022-05-12 14:00:08 +0200

    correzioni css

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //Inserire in un array gli elementi da riprodurre
 
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
+    'img/NY.jpg',
+    'img/RM.jpg',
+    'img/RJ.jpg',
+    'img/SH.jpg',
+    'img/LN.jpg'
 ];
   
 const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
+    'New York',
+    'Roma',
+    'Rio de Janeiro',
+    'Shangai',
+    'Londra'
 ]
   
 const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    'New York è una città degli Stati Uniti d\'America situata nello stato federato di New York. Si estende su una superficie di 1200 kmq ed è di circa 8.7 milioni di abitanti per una densità di popolazione di circa 6.9 mila abitanti per chilometro quadro.',
+    'Roma, capitale d\'Italia, è considerata una delle più belle città del mondo. Il suo centro storico, insieme alle proprietà extraterritoriali della Santa Sede dentro la città e alla Basilica di San Paolo Fuori le Mura, è tra i 58 siti italiani inseriti dall\'Unesco nella World Heritage List.',
+    'Natura esuberante, cultura viva e variegata, abitanti calorosi e ospitali, tanta storia e un\infinità di contrasti: sono queste le ragioni che fanno di Rio de Janeiro quell\'icona celebre in tutto il mondo e, senza ombra di dubbio, la città latinoamericana più interessante da visitare.',
+    'Shanghai è la città più popolata della Cina e la terza a livello mondiale. Considerata la capitale economica del Paese, grazie allo sviluppo degli ultimi due decenni, è un centro economico, finanziario, commerciale e delle comunicazioni di primaria importanza mondiale.',
+    'La città è situata nella zona meridionale della Gran Bretagna. Londra è una città di importanza mondiale dal punto di vista culturale, economico, finanziario. E\' la terza piazza borsistica del mondo. Londra è anche una città che detta le principali influenze in campo culturale,politico ,economico ed artistico.',
 ]
   
 //Dichiarare dove inserire gli elementi
 const imgSmall = document.querySelector('div.img_small');
 const imgFull = document.querySelector('div.img_full');
   
-let selected = 3;
+let selected = 0;
 imgFull.innerHTML = `<img src="${items[selected]}">`
   
 //Creare ciclo per inserire gli elementi
